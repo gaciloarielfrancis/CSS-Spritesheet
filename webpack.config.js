e3aff69051d2de57a3a971cd3109c90802340655
Ariel Francis Gacilo <gaciloarielfrancis@gmail.com>
2024-03-29 18:42:17 +0800

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
-var mode = process.env.NODE_ENV || 'development';
 module.exports = {
-    devtool: (mode === 'development') ? 'inline-source-map' : false,
-    mode: mode,
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new CopyPlugin({
+          patterns: [
+            { from: "public" },
+          ],
+        }),
+    ],
 };
